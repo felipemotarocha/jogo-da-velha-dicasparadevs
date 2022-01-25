86edab7285b5cd41ca550c227b513e512713c456
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-25 16:54:02 -0300

    initial commit

---
+++ index.html
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./styles.css" />
+    <title>EM PROGRESSO - Jogo da Velha</title>
+  </head>
+  <body>
+    <div class="board" data-board>
+      <div class="cell" data-cell></div>
+      <div class="cell" data-cell></div>
+      <div class="cell" data-cell></div>
+      <div class="cell" data-cell></div>
+      <div class="cell" data-cell></div>
+      <div class="cell" data-cell></div>
+      <div class="cell" data-cell></div>
+      <div class="cell" data-cell></div>
+      <div class="cell" data-cell></div>
+    </div>
+
+    <div class="winning-message" data-winning-message>
+      <p class="winning-message-text" data-winning-message-text>X Venceu!</p>
+      <button class="winning-message-button" data-restart-button>
+        Reiniciar!
+      </button>
+    </div>
+    <script src="./scripts.js"></script>
+  </body>
+</html>
---
+++ scripts.js
@@ -0,0 +1,108 @@
+const cellElements = document.querySelectorAll("[data-cell]");
+const board = document.querySelector("[data-board]");
+const winningMessageTextElement = document.querySelector(
+  "[data-winning-message-text]"
+);
+const winningMessage = document.querySelector("[data-winning-message]");
+const restartButton = document.querySelector("[data-restart-button]");
+
+let isCircleTurn;
+
+const winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+const startGame = () => {
+  isCircleTurn = false;
+
+  for (const cell of cellElements) {
+    cell.classList.remove("circle");
+    cell.classList.remove("x");
+    cell.removeEventListener("click", handleClick);
+    cell.addEventListener("click", handleClick, { once: true });
+  }
+
+  setBoardHoverClass();
+  winningMessage.classList.remove("show-winning-message");
+};
+
+const endGame = (isDraw) => {
+  if (isDraw) {
+    winningMessageTextElement.innerText = "Empate!";
+  } else {
+    winningMessageTextElement.innerText = isCircleTurn
+      ? "O Venceu!"
+      : "X Venceu!";
+  }
+
+  winningMessage.classList.add("show-winning-message");
+};
+
+const checkForWin = (currentPlayer) => {
+  return winningCombinations.some((combination) => {
+    return combination.every((index) => {
+      return cellElements[index].classList.contains(currentPlayer);
+    });
+  });
+};
+
+const checkForDraw = () => {
+  return [...cellElements].every((cell) => {
+    return cell.classList.contains("x") || cell.classList.contains("circle");
+  });
+};
+
+const placeMark = (cell, classToAdd) => {
+  cell.classList.add(classToAdd);
+};
+
+const setBoardHoverClass = () => {
+  board.classList.remove("circle");
+  board.classList.remove("x");
+
+  if (isCircleTurn) {
+    board.classList.add("circle");
+  } else {
+    board.classList.add("x");
+  }
+};
+
+const swapTurns = () => {
+  isCircleTurn = !isCircleTurn;
+
+  setBoardHoverClass();
+};
+
+const handleClick = (e) => {
+  // Colocar a marca (X ou Círculo)
+  const cell = e.target;
+  const classToAdd = isCircleTurn ? "circle" : "x";
+
+  placeMark(cell, classToAdd);
+
+  // Verificar por vitória
+  const isWin = checkForWin(classToAdd);
+
+  // Verificar por empate
+  const isDraw = checkForDraw();
+
+  if (isWin) {
+    endGame(false);
+  } else if (isDraw) {
+    endGame(true);
+  } else {
+    // Mudar símbolo
+    swapTurns();
+  }
+};
+
+startGame();
+
+restartButton.addEventListener("click", startGame);
